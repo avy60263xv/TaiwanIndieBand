ac210fef8e0f95e2f8a7902f2eead03bc4e027a3
avy60263xv <avy60263xv@gmail.com>
2021-02-23 00:18:37 +0800

    NEW BAND SESSTION

---
+++ slider.js
@@ -0,0 +1,9 @@
+document.addEventListener( 'DOMContentLoaded', function () {
+    new Splide( '.splide', {
+        perPage: 4,
+        rewind : true,
+        perMove:1,
+        width: 2500,
+        
+    } ).mount();
+} );
