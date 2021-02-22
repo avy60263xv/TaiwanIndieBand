6f2736902e0a695418da25fd5460a889ec735993
avy60263xv <avy60263xv@gmail.com>
2021-02-23 00:33:32 +0800

    新增 Slider RWD

--- slider.js
+++ slider.js
@@ -1,9 +1,29 @@
+const ScreenWidth =  document.body.offsetWidth;
+console.log(ScreenWidth);
 document.addEventListener( 'DOMContentLoaded', function () {
+    if (ScreenWidth<=414){
+        SmallSlider();
+    }else{
+        BigSlider();
+    }
+} );
+
+function BigSlider(){
     new Splide( '.splide', {
         perPage: 4,
         rewind : true,
         perMove:1,
         width: 2500,
-        
+        pagination: false
     } ).mount();
-} );
+}
+
+function SmallSlider(){
+    new Splide( '.splide', {
+        perPage: 1,
+        rewind : true,
+        perMove:1,
+        width: 2500,
+        pagination: false
+    } ).mount();
+}
