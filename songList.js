a4ebe2624677b1f72d35fa96630771f81292509f
avy60263xv <avy60263xv@gmail.com>
2021-02-27 14:34:57 +0800

    localStorage show and add

--- songList.js
+++ songList.js
@@ -21,6 +21,7 @@ function ChangePageSongs(){
       });    
 }
 
+//html骨架
 function showSongList(songsList){
     let songs = document.getElementById("songs");
     var songList = '';
@@ -37,11 +38,23 @@ function showSongList(songsList){
         songList += songsHtml;
     }
     //console.log(songList);
+    //顯示不同樂團的Banner
     songs.innerHTML = `<div class="songBanner m-3"><img src="img/${songsList.BandName}/${songsList.BandName}Banner.jpg" alt=""></div>${songList}`;
 }
 
-var buyingList = []; //存有購買的歌曲，用物件存，購物車列表
+//撈localStorage的資料（先從字串轉回陣列-物件格式，在存到buyingList重新裡面）
+var getmyShoppingList = localStorage.getItem('myShoppingList');
+var buyingList; //存有購買的歌曲，用物件存，購物車列表
+if(getmyShoppingList){
+    buyingList = JSON.parse(getmyShoppingList);
+    console.log(buyingList);
+    Showbuyitmes(buyingList);
+}else{
+    buyingList = [];
+}
 
+
+//購物車滑動出現/消失
 const MycartIcon = document.getElementById("Mycart");
 MycartIcon.addEventListener("mouseenter", function(){
     document.getElementById("MyCart").style.display = 'block';
@@ -63,8 +76,22 @@ function additem(BandName, songName, price){
         songName: songName,
         price: price
     };
-    buyingList.push(song); //購買一筆歌曲
-    //console.log(buyingList);
+    
+    // 新增並過濾相同的物件
+    buyingList.push(song);
+    const set = new Set();
+   // buyingList =  buyingList.filter(item => !set.has(item.songName) ? set.add(item.songName) : false); //把第一次看到的物件留下來（用歌名比對）
+    buyingList =  buyingList.filter(function(item){
+        if(!set.has(item.songName)){
+            return set.add(item.songName);
+        }else{
+            alert("此歌曲已加入購物車囉！");
+        }
+    }); 
+    console.log(buyingList);
+    //同時新增資料到localStorage(物件轉字串，存進localStorage)
+    var buyingListString = JSON.stringify(buyingList);
+    localStorage.setItem('myShoppingList',buyingListString);
     Showbuyitmes(buyingList);
 }
 
@@ -72,21 +99,23 @@ function additem(BandName, songName, price){
 function deleteitem(deletesong){
     buyingList.splice(deletesong,1)
     //console.log(buyingList);
+    ////同時刪除localStorage中的該筆資料
     Showbuyitmes(buyingList);
     
 }
 
-function Showbuyitmes(item){
+function Showbuyitmes(buyingList){
     let ShowCart = document.getElementById("MyCart");
+
     var addCartHtml=''; 
-    for(let i=0; i<item.length; i++){
+    for(let i=0; i<buyingList.length; i++){
         //addCartHtml += `<tr><td>${item[i].songName}</td> <td>${item[i].price}</td> <td><button id="delete" onclick="deleteitem(${i})">X</button></td></tr>`; //新增html語法 / 如果該項刪除鈕被點下去，去執行刪除function
         addCartHtml += `      
         <div class="item">
-            <img src="img/${item[i].BandName}/${item[i].songName}.jpg" alt="">
-            <span class="songName">${item[i].songName}</span>
-            <span class="bandName">${item[i].BandName}</span>
-            <span class="price">$${item[i].price}</span>
+            <img src="img/${buyingList[i].BandName}/${buyingList[i].songName}.jpg" alt="">
+            <span class="songName">${buyingList[i].songName}</span>
+            <span class="bandName">${buyingList[i].BandName}</span>
+            <span class="price">$${buyingList[i].price}</span>
             <img class="cancel" onclick="deleteitem(${i})" src="img/icon/cancel.png" alt="">
         </div>`; //新增html語法 / 如果該項刪除鈕被點下去，去執行刪除function
     }
