b6c3f4b1a83ad35587feb7864e2dd58fb682d058
avy60263xv <avy60263xv@gmail.com>
2021-03-01 14:07:46 +0800

    APP頁面 完成

---
+++ sliderPhone.js
@@ -0,0 +1,17 @@
+new Splide( '.splide', {
+    perPage: 3,
+    rewind : true,
+    perMove:1,
+    width: 800,
+    focus: 'center',
+	perPage: 3,
+	trimSpace: false,
+    pagination: true,
+    autoplay: true,
+    start: 1,
+    arrows: false,
+    padding: {
+		left : 0,
+		right: '6rem',
+	}
+} ).mount();
