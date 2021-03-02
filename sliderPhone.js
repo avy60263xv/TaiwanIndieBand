adaeee8dfa8d48a3647de60336212a29af23b040
avy60263xv <avy60263xv@gmail.com>
2021-03-02 23:38:19 +0800

    修改 APP splide RWD

--- sliderPhone.js
+++ sliderPhone.js
@@ -1,17 +1,49 @@
-new Splide( '.splide', {
-    perPage: 3,
-    rewind : true,
-    perMove:1,
-    width: 800,
-    focus: 'center',
-	perPage: 3,
-	trimSpace: false,
-    pagination: true,
-    autoplay: true,
-    start: 1,
-    arrows: false,
-    padding: {
-		left : 0,
-		right: '6rem',
-	}
-} ).mount();
+const ScreenWidth =  document.body.offsetWidth;
+//console.log(ScreenWidth);
+document.addEventListener( 'DOMContentLoaded', function () {
+    if (ScreenWidth<=414){
+        SmallSlider();
+    }else{
+        BigSlider();
+    }
+} );
+
+function BigSlider(){
+    new Splide( '.splide', {
+        perPage: 3,
+        rewind : true,
+        perMove:1,
+        width: 800,
+        focus: 'center',
+        perPage: 3,
+        trimSpace: false,
+        pagination: true,
+        autoplay: true,
+        start: 1,
+        arrows: false,
+        padding: {
+            left : 0,
+            right: '6rem',
+        }
+    } ).mount();
+}
+
+function SmallSlider(){
+    new Splide( '.splide', {
+        perPage: 3,
+        rewind : true,
+        perMove:1,
+        width: 800,
+        focus: 'center',
+        perPage: 1,
+        trimSpace: false,
+        pagination: true,
+        autoplay: true,
+        start: 1,
+        arrows: false,
+        padding: {
+            left : 0,
+            right: '6rem',
+        }
+    } ).mount();
+}
